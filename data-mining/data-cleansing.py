bbb65db4c90d81bcb400fe8e10631b59e9e4ead5
hongyu <hy19950714@outlook.com>
2017-05-08 07:46:28 +0800

    测试

--- data-mining/data-cleansing.py
+++ data-mining/data-cleansing.py
@@ -28,24 +28,41 @@ def is_my_ip(ip):
     return ip in s
 
 
-source_file = open("/Users/hongyu/Desktop/2016-08-05-05-new.txt", 'rb')
+source_file = open("/toshibaVolume/BISTU-NETWORK-DATA/2016-08-07-new/2016-08-07-23-new.txt", 'rb')
 count = 0
 count2 = 1
 dstipList = []
 newf = file("first-cleansing.txt", "w+")
+old_timestamp = 0
+itemset = ''
 for line in source_file:
     # if count < 1000000:
+
     try:
         newLine = line.split(',')
-        if is_my_ip(str(newLine[0])):
-            if str(newLine[7]) == 'HTTP' and str(newLine[6]) == 'HTTP':
-                if str(newLine[12]) != '':
-                    # dstipList.append(newLine[0])
-                    # print count2, str(line[0]),  str(line[12])
-                    # print line
-                    # str(line[6]), str(line[7]), str(line[8]), str(line[9]),
-                    newf.writelines(line)
-                    # count2 += 1
+
+        new_timestamp = str(newLine[4])
+        print 'new:' + str(new_timestamp)
+        print 'old:' + str(old_timestamp)
+        if (int(new_timestamp) - int(old_timestamp)) > 10:
+            print itemset
+            newf.writelines(itemset)
+            itemset = ''
+            old_timestamp = str(newLine[4])
+            #  if is_my_ip(str(newLine[0])):
+        if str(newLine[7]) == 'HTTP' and str(newLine[6]) == 'HTTP':
+            if str(newLine[12]) != '':
+                # dstipList.append(newLine[0])
+                # print count2, str(line[0]),  str(line[12])
+                # print line
+                # str(line[6]), str(line[7]), str(line[8]), str(line[9]),
+
+                # cleansed_line = str(newLine[12])+','+str(newLine[4]) + ','
+
+                itemset += str(newLine[12]) + ','
+#                print itemset
+                #newf.writelines(cleansed_line)
+                # count2 += 1
     except Exception, e:
         print e
     count += 1
